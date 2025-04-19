328d1c3bba7a28f00236f6ce8e62cdc16a50c829
Dehkia Samani Khouzani <166752234+DehkiaSK@users.noreply.github.com>
2025-04-19 16:31:49 +0330

    My first official commit to my repository

---
+++ Binary_Search/74_Search_2D_Matrix/MyAnswer/Answer.cs
@@ -0,0 +1,40 @@
+﻿namespace MyAnswer;
+public class Answer
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        int rows = matrix.Length;
+        int columns = matrix[0].Length;
+
+        int low = 0;
+        int high = (rows * columns) - 1;
+
+        while(low <= high)
+        {
+            int mid = low + ((high - low) / 2);
+
+            int rowIndex = mid / columns;
+            //Represents the row index of the element in the matrix
+            //where the mid index points
+
+            int columnIndex = mid % columns;
+            //Represents the column index of the same element within
+            //that row
+
+            if(matrix[rowIndex][columnIndex] == target)
+            {
+                return true;
+            }
+            else if(matrix[rowIndex][columnIndex] < target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Binary_Search/74_Search_2D_Matrix/MyAnswer/Program.cs
@@ -0,0 +1,9 @@
+﻿using MyAnswer;
+
+Answer answer = new Answer();
+
+int[][] myMatrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]];
+
+bool result = answer.SearchMatrix(myMatrix, 35);
+
+Console.WriteLine(result);
