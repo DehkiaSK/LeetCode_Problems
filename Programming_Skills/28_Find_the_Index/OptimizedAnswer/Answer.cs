f61d9e80a06d012a87e5b642d74c886751665c28
Dehkia Samani Khouzani <166752234+DehkiaSK@users.noreply.github.com>
2025-07-11 06:21:14 +0330

    28_Find_the_Index
    Find the index of the first occurrence in a string.

---
+++ Programming_Skills/28_Find_the_Index/OptimizedAnswer/Answer.cs
@@ -0,0 +1,22 @@
+﻿public class Answer
+{
+    public int StrStr(string haystack, string needle) 
+    {
+        if(needle.Length == 0)
+        {
+            return 0;
+        }
+
+        for(int i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            string window = haystack.Substring(i, needle.Length);
+
+            if(window == needle)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ Programming_Skills/28_Find_the_Index/OptimizedAnswer/Program.cs
@@ -0,0 +1,8 @@
+﻿Answer answer = new();
+
+string haystack = "leetcode";
+string needle = "leeto";
+
+int result = answer.StrStr(haystack, needle);
+
+Console.WriteLine(result);
