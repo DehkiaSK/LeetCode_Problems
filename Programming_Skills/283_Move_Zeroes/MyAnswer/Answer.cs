4c61f93a0cbc0c233470194cc540e4845116e6fd
Dehkia Samani Khouzani <166752234+DehkiaSK@users.noreply.github.com>
2025-07-13 12:50:56 +0330

    283_Move_Zeroes

---
+++ Programming_Skills/283_Move_Zeroes/MyAnswer/Answer.cs
@@ -0,0 +1,39 @@
+﻿namespace MyAnswer;
+
+public class Answer
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int insertPos = 0;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[insertPos] = nums[i];
+
+                insertPos++;
+            }
+
+            //Console.WriteLine
+            //foreach(int x in nums)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //Console.WriteLine();
+        }
+
+        while(insertPos < nums.Length)
+        {
+            nums[insertPos] = 0;
+
+            insertPos++;
+        }
+
+        foreach(int x in nums)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Programming_Skills/283_Move_Zeroes/MyAnswer/Program.cs
@@ -0,0 +1,7 @@
+﻿using MyAnswer;
+
+Answer answer = new();
+
+int[] array = [2, 0, -1, 0, 4, 1];
+
+answer.MoveZeroes(array);
