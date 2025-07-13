64bd963e89f9254a756667dd1888536e50d60a02
Dehkia Samani Khouzani <166752234+DehkiaSK@users.noreply.github.com>
2025-07-13 12:49:10 +0330

    242_Valid_Anagram

---
+++ Programming_Skills/242_Valid_Anagram/MyAnswer/Answer.cs
@@ -0,0 +1,50 @@
+﻿namespace MyAnswer;
+
+public class Answer
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if(s.Length != t.Length)
+        {
+            return false;
+        }
+
+        Dictionary<char, int> sDictionary = new();
+        Dictionary<char, int> tDictionary = new();
+
+        foreach(char c in s)
+        {
+            if(sDictionary.ContainsKey(c))
+            {
+                sDictionary[c]++;
+            }
+            else
+            {
+                sDictionary[c] = 1;
+            }
+        }
+
+        foreach(char c in t)
+        {
+            if(tDictionary.ContainsKey(c))
+            {
+                tDictionary[c]++;
+            }
+            else
+            {
+                tDictionary[c] = 1;
+            }
+        }
+
+        foreach(var item in sDictionary)
+        {
+            if(!tDictionary.ContainsKey(item.Key) || tDictionary[item.Key] != item.Value)
+            {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}
---
+++ Programming_Skills/242_Valid_Anagram/MyAnswer/Program.cs
@@ -0,0 +1,11 @@
+﻿using MyAnswer;
+
+Answer answer = new();
+
+string s = "shoo";
+string t = "something";
+bool result = false;
+
+result = answer.IsAnagram(s, t);
+
+Console.WriteLine(result);
